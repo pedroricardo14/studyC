3be4d88afbe9c6bf9b378052868ff03f827360fd
pedroricardo14 <pedroricardo1032@gmail.com>
2025-06-05 17:48:00 -0300

    Aula sobre os comandos break e continue

---
+++ comandos-break-continue.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+
+    for(i = 1; i <= 10; i++){
+        printf("%d", i);
+        if (i == 5){
+            break;
+        }
+    }
+
+    for(i = 1; i <= 10; i++){
+        if (i == 5){
+            continue;
+        }
+        printf("%d", i);
+    }
+}
